e3ccae5500497826e25931b10a11dc14ff089c9c
Sudhanshu Jaisani <sudhanshujaisani38@gmail.com>
2020-05-13 20:29:33 +0530

    MinQuery with lazy population(Segment Trees)

--- CB/Segment Trees/MinQuery.java
+++ CB/Segment Trees/MinQuery.java
@@ -9,27 +9,129 @@ class MinQuery {
         int noOfQueries = sc.nextInt();
         int arr[] = new int[n];
         int segtree[] = new int[4 * n + 1]; // for n elements segment tree can use max of 4n+1 elements
+        int lazyValue[] = new int[segtree.length];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         buildSegmentTree(arr, segtree, 0, n - 1, 1);
-        
+
         for (int i = 0; i < noOfQueries; i++) {
+            for (int k = 0; k < segtree.length; k++) {
+                System.out.print(segtree[k] + ",");
+            }
+            System.out.println();
             int queryType = sc.nextInt();
             if (queryType == 1) {
                 int l = sc.nextInt();
                 int r = sc.nextInt();
-                l--;r--;
-                int ans = query(segtree, l, r, 0, n - 1, 1);
+                l--;
+                r--;
+                // int ans = query(segtree, l, r, 0, n - 1, 1);
+                int ans=queryLazy(segtree,lazyValue,l,r,0,n-1,1);
                 System.out.println(ans);
-            }else{
-                int index=sc.nextInt();
+            } else if (queryType == 2) {
+                int index = sc.nextInt();
                 index--;
-                int newValue=sc.nextInt();
-                arr[index]=newValue;
-                updateElement(index, 1, newValue, segtree, 0, n-1);
-               
+                int newValue = sc.nextInt();
+                arr[index] = newValue;
+                updateNode(index, 1, newValue, segtree, 0, n - 1);
+            } else if (queryType == 3) { // updating an entire range
+                int l = sc.nextInt();
+                int r = sc.nextInt();
+                int inc = sc.nextInt();
+                l--;
+                r--;
+                updateRangeLazy(segtree, lazyValue, l, r, inc, 0, n - 1, 1);
+                // updateRange(segtree,l,r,inc,0,n-1,1);
+
             }
+        }
+    }
+
+    private static int queryLazy(int[] segtree, int[] lazyValue, int l, int r, int ns, int ne, int index) {
+        //first resolve the lazy value at this node, then do anything.
+        if(lazyValue[index]!=0){
+            segtree[index]+=lazyValue[index];
+            if(ns!=ne){
+                lazyValue[2*index]+=lazyValue[index];
+                lazyValue[2*index+1]+=lazyValue[index];
+            }
+            lazyValue[index]=0;
+        }
+        if(r<ns || l>ne){
+            //no overlap
+            return Integer.MAX_VALUE;
+        }
+        if(ns>=l && ne<=r){
+            //complete overlap
+            return segtree[index];
+        }else{
+            //partial overlap
+            int mid=(ns+ne)/2;
+            int leftAns=queryLazy(segtree, lazyValue, l, r, ns, mid, 2*index);
+            int rightAns=queryLazy(segtree, lazyValue, l, r, mid+1, ne, 2*index+1);
+            return Math.min(leftAns,rightAns);
+        }
+    }
+
+    private static void updateRangeLazy(int[] segtree, int[] lazyValue, int l, int r, int inc, int ns, int ne,
+            int index) {
+        // before going down,resolve the lazy value here & propogate it to left&right if
+        // it's not a leaf node
+        if (lazyValue[index] != 0) {
+            segtree[index] += lazyValue[index];
+            if (ns != ne) {
+                lazyValue[2 * index] += lazyValue[index];
+                lazyValue[2 * index + 1] += lazyValue[index];
+            }
+            lazyValue[index] = 0;
+        }
+
+        if (r < ns || l > ne) {
+            return;
+        }
+        if (ns == ne) {
+            segtree[index] += inc;
+            return;
+        }
+        if (ns >= l && ne <= r) {
+            // complete overlap - update this node, pass the lazy value(if not leaf) &
+            // return
+            segtree[index] += inc;
+            if (ns != ne) {
+                lazyValue[2 * index] += inc;
+                lazyValue[2 * index + 1] += inc;
+            }
+            // return from here only.
+            return;
+        } else {
+            //partial overlap-call on left& right
+            int mid = (ns + ne) / 2;
+            updateRangeLazy(segtree, lazyValue, l, r, inc, ns, mid, 2 * index);
+            updateRangeLazy(segtree, lazyValue, l, r, inc, mid + 1, ne, 2 * index + 1);
+            segtree[index]=Math.min(segtree[2*index],segtree[2*index+1]);
+        }
+
+    }
+
+
+
+    // increments the numbers in range from l to r by inc.
+    private static void updateRange(int[] segtree, int l, int r, int inc, int ns, int ne, int index) {
+
+        if (r < ns || l > ne) {
+            // no overlap-do nothing
+            return;
+        }
+        if (ns == ne) {
+            segtree[index] += inc;
+            return;
+        } else {
+            // partial or complete overlap - call on left & right
+            int mid = (ns + ne) / 2;
+            updateRange(segtree, l, r, inc, ns, mid, 2 * index);
+            updateRange(segtree, l, r, inc, mid + 1, ne, 2 * index + 1);
+            segtree[index] = Math.min(segtree[2 * index], segtree[2 * index + 1]);
         }
     }
 
@@ -68,19 +170,19 @@ class MinQuery {
         return segtree[treeIndex];
     }
 
-    private static void updateElement(int arrIndex, int treeIndex, int newValue, int segtree[], int ns, int ne) {
+    private static void updateNode(int arrIndex, int treeIndex, int newValue, int segtree[], int ns, int ne) {
         if (arrIndex == ns && arrIndex == ne) {
             segtree[treeIndex] = newValue;
         } else {
             // rec case
-            
+
             int mid = (ns + ne) / 2;
             if (arrIndex <= mid) {
-                updateElement(arrIndex, 2 * treeIndex, newValue, segtree, ns, mid);
+                updateNode(arrIndex, 2 * treeIndex, newValue, segtree, ns, mid);
             } else {
-                updateElement(arrIndex, 2 * treeIndex + 1, newValue, segtree, mid + 1, ne);
+                updateNode(arrIndex, 2 * treeIndex + 1, newValue, segtree, mid + 1, ne);
             }
-            segtree[treeIndex]=Math.min(segtree[2*treeIndex],segtree[2*treeIndex+1]);
+            segtree[treeIndex] = Math.min(segtree[2 * treeIndex], segtree[2 * treeIndex + 1]);
         }
     }
 }
