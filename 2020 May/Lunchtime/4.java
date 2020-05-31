d728da52702a9e58157b4678943af987ab5d7ce2
Sudhanshu Jaisani <sudhanshujaisani38@gmail.com>
2020-05-31 22:55:34 +0530

    Convstr (may Ltime20) using editorial

--- 2020 May/Lunchtime/4.java
+++ 2020 May/Lunchtime/4.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 class Main {
@@ -16,35 +15,66 @@ class Main {
         try {
             int testCases = fastReader.nextInt();
             while (testCases-- > 0) {
-                int n=fastReader.nextInt();
-                String s1=fastReader.next();
-                String s2=fastReader.next();
-                HashMap<Character,ArrayList<Integer>> map1=new HashMap<>();
-                HashMap<Character,ArrayList<Integer>> map2=new HashMap<>();
-                for(int i=0;i<26;i++){
-                    char c=(char)('a'+i);
-                    map1.put(c,new ArrayList<>());
-                    map2.put(c,new ArrayList<>());
-                }
-                for(int i=0;i<n;i++){
-                    char c1=s1.charAt(i);
-                    char c2=s2.charAt(i);
-                    map1.get(c1).add(i);
-                    map2.get(c2).add(i);
-                }
-                for(int i=0;i<26;i++){
-                    char c=(char)('a'+i);
-                    if(map2.get(c).size()!=0){
-                        if(map1.get(c).size()==0){
-                            stringBuffer.append("-1\n");
-                        }
-                    }
-                }
+                int n = fastReader.nextInt();
+                String s1 = fastReader.next();
+                String s2 = fastReader.next();
+                solve(n, s1, s2);
             }
             bufferedWriter.write(stringBuffer.toString());
             bufferedWriter.flush();
         } catch (Exception e) {
             return;
+        }
+    }
+
+    private static void solve(int n, String s1, String s2) {
+        for (int i = 0; i < n; i++) {
+            if (s1.charAt(i) < s2.charAt(i)) {
+                System.out.println(-1);
+                return;
+            }
+        }
+        boolean isPresent[] = new boolean[26]; // stores if a character is present in s1 or not
+        for (int i = 0; i < n; i++) {
+            char c = s1.charAt(i);
+            isPresent[c - 'a'] = true;
+        }
+        for (int i = 0; i < n; i++) {
+            char c = s2.charAt(i);
+            if (!isPresent[c - 'a']) {
+                System.out.println(-1);
+                return ;
+            }
+        }
+        ArrayList<ArrayList<Integer>> operations=new ArrayList<>();
+        for (char c = 'z'; c >= 'a'; c--) {
+            ArrayList<Integer> set = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                if (s2.charAt(i) == c && s1.charAt(i) != c) {
+                    set.add(i);
+                }
+            }
+            if (set.size() > 0) {
+                for (int i = 0; i < n; i++) {
+                    if(s1.charAt(i)==c){
+                        set.add(i);
+                    }
+                }
+                operations.add(set);
+            }
+            for(int i=0;i<set.size();i++){
+                int index=set.get(i);
+                s1=s1.substring(0, index)+c+s1.substring(index+1);
+            }
+        }
+        System.out.println(operations.size());
+        for(int i=0;i<operations.size();i++){
+            ArrayList<Integer> operation=operations.get(i);
+            System.out.print(operation.size());
+            for(int j=0;j<operation.size();j++){
+                System.out.print(" "+operation.get(j));
+            }
+            System.out.println();
         }
     }
 
