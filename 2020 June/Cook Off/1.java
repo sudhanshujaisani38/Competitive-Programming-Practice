26cf9422699623b75ed473d0aee2f8715c4a636e
Sudhanshu Jaisani <sudhanshujaisani38@gmail.com>
2020-06-21 16:05:31 +0530

    June 2020 Cook-Off

---
+++ 2020 June/Cook Off/1.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Main {
+    static FastReader fastReader = new FastReader();
+    static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer stringBuffer = new StringBuffer();
+
+    public static void main(String[] args) {
+        try {
+            int testCases = fastReader.nextInt();
+            while (testCases-- > 0) {
+            }
+            bufferedWriter.write(stringBuffer.toString());
+            bufferedWriter.flush();
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int[] inputIntegerArray(int size) {
+            int arr[] = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = nextInt();
+            }
+            return arr;
+        }
+
+        long[] inputLongArray(int size) {
+            long arr[] = new long[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = nextLong();
+            }
+            return arr;
+        }
+
+        double[] inputDoubleArray(int size) {
+            double arr[] = new double[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = nextDouble();
+            }
+            return arr;
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
