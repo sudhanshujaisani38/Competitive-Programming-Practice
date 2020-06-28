cea31e0e8fd225df56cd42697a4544d21b630245
Sudhanshu Jaisani <sudhanshujaisani38@gmail.com>
2020-06-28 11:59:25 +0530

    GLOMINE(June Lunchtime using editorial)

---
+++ 2020 June/Lunchtime/3.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.util.StringTokenizer;
+
+class Main {
+    static {
+        try {
+            System.setOut(new PrintStream(new File("output.txt")));
+            new File("input.txt").createNewFile();
+            System.setIn(new FileInputStream(new File("input.txt")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    static FastReader fastReader = new FastReader();
+    static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    static StringBuffer stringBuffer = new StringBuffer();
+
+    public static void main(String[] args) throws Exception {
+        try {
+            int testCases = fastReader.nextInt();
+            // int testCases=1;
+            while (testCases-- > 0) {
+                int n = fastReader.nextInt();
+                double g[] = new double[n];
+                double a[] = new double[n];
+                double b[] = new double[n];     
+                double ansA=0,ansB=0;       
+                for (int i = 0; i < n; i++) {
+                    g[i] = fastReader.nextDouble();
+                    a[i] = fastReader.nextDouble();
+                    b[i] = fastReader.nextDouble();
+                    double sum=a[i]+b[i];
+                    ansA=ansA+(g[i]*a[i])/sum;
+                    ansB=ansB+(g[i]*b[i])/sum;
+                }
+                System.out.printf("%.10f %.10f\n",ansB,ansA);           
+            }
+            bufferedWriter.write(stringBuffer.toString());
+            bufferedWriter.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+    }
+}
