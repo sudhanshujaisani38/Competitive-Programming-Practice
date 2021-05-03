6eabb40f6bea2bf8dde4c420099104ffb031c284
Sudhanshu Jaisani <sudhanshu.jaisani@clear-trail.com>
2021-05-03 21:34:10 +0530

    Leetcode(May-3) Prefix sum

--- Leetcode/Adhoc/RunningSum.java
+++ Leetcode/Adhoc/RunningSum.java
@@ -1,3 +1,7 @@
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+*/
 class Solution {
     public int[] runningSum(int[] nums) {
         int len = nums.length;
