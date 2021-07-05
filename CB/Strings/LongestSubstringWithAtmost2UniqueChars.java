938858c08a815321d3bc374cb3bd04ec288bbad1
Sudhanshu Jaisani <sudhanshujaisani38@gmail.com>
2021-07-05 22:51:00 +0530

    String & Sliding Window: Longest Substring with atmost 2 unique chars (comments)

--- CB/Strings/LongestSubstringWithAtmost2UniqueChars.java
+++ CB/Strings/LongestSubstringWithAtmost2UniqueChars.java
@@ -16,18 +16,26 @@ public class LongestSubstringWithAtmost2UniqueChars {
         int count[] = new int[128];
         int ans = 0;
         while (right < s.length()) {
+
+            // add the rightmost char and increase frequency
             set.add(charAt[right]);
             count[charAt[right]]++;
+
+            // keep advancing the left untill only 2 distinct chars are present in the range
             while (set.size() > 2 && left < right) {
+                // advance the left and decrease frequency
                 char leftChar = charAt[left];
                 count[leftChar]--;
                 left++;
-                if (count[leftChar] == 0)
+                if (count[leftChar] == 0)// if one of the char is completely removed, remove it from the set
                     set.remove(leftChar);
             }
+
             if (set.size() <= 2) {
+                // update the ans if the requirment meets
                 ans = Math.max(ans, right - left + 1);
             }
+
             right++;
         }
         return ans;
